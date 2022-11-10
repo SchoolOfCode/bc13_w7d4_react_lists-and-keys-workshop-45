de4b8336fef020177b0424a62be6d7db6da7d830
mohamedabdi <mohamed.abdi11@hotmail.co.uk>
2022-11-10 16:52:13 +0000

    tasks completed

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
-function Post({text}) {
-  return <article>{text}</article>;
+function Post({ text, author, date, title, image, highlights }) {
+  return (
+    <article>
+      <h1>{title}</h1>
+      <h2>{date}</h2>
+      <img src={image.link} alt={image.alt}></img>
+      <p>{text}</p>
+      <h3>{author}</h3>
+      {highlights.map((highlight) => {
+        return <li>{highlight}</li>;
+      })}
+    </article>
+  );
 }
 
 export default Post;
